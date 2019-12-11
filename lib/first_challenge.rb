6c889393c490ac5d914667a50d31ec43ba883822
Aidan Lincoln <aidantlincoln@gmail.com>
2019-12-11 21:19:05 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,9 +13,16 @@ def first_challenge
        :name => "Romeo"
        :age => "15"
        :status => "alive"
-       
      }
-     :hero_friends => []
+     :hero_friends => [{
+       :name => "Benvolio"
+       :age => "17"
+       :attitude => "worried"
+     }{
+       :name => "Mercutio"
+       :age => "18"
+       :attitude => "hot-headed"
+     }]
    }
    :capulet => {
      :patriarch => {
